85d98931a8af2cbc07334e0d7fb07254bd6ec99d
Şule Şinikoğlu <sulesinikoglu@sulesinikoglu.local>
2021-01-28 03:26:52 +0300

    add script command

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 9000;
 
-app.get("/", (req, res) => res.send("hello world"));
+app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port);
 
 console.log(`[app] : http://localhost:${port}`);
